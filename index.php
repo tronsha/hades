65e00e2c23da11bc723131c0f661036b65201111
Stefan Hüsges <Tronsha@gmx.de>
2014-09-12 11:04:35 +0200

    Update index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Hades</title>
+        <meta name="description" content="Hades, master of Cerberus.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    </body>
+</html>
