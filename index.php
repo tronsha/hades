7e9ee6ddaf81156f074e5d90f776718757313953
Stefan Hüsges <Tronsha@gmx.de>
2014-11-13 10:21:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
         <title>Hades</title>
         <meta name="description" content="Hades, master of Cerberus.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        
     </body>
 </html>
