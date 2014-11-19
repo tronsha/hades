2c4a2aa4f617d3739620fa1543da563148b2c594
Stefan Hüsges <Tronsha@gmx.de>
2014-11-19 08:48:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Hades</title>
-        <meta name="description" content="Hades, master of Cerberus.">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Chat - Hades</title>
+    <meta name="description" content="Hades, master of Cerberus">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='//fonts.googleapis.com/css?family=Calligraffitti' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="chat">
+    <h1>Hades</h1>
+    <div class="title"></div>
+    <div class="output"></div>
+    <div class="input">
+        <input id="input" placeholder="$">
+    </div>
+    <div class="channels list"></div>
+    <div class="users list"></div>
+    <div class="buttons">
+        <span class="fa fa-comment"></span>
+        &nbsp;
+        <span class="fa fa-users"></span>
+        &nbsp;
+        <span class="fa fa-sign-out"></span>
+    </div>
+    <div class="submit">
+        <span class="fa fa-send"></span>
+    </div>
+</div>
+</body>
 </html>
