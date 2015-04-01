6589853533a7818c97a00d94f01271d69cb804c8
Stefan Hüsges <tronsha@gmx.de>
2015-04-01 11:12:11 +0200

    remove most cdn

--- index.php
+++ index.php
@@ -28,9 +28,9 @@ $_SESSION['last'] = 0;
     <title>Chat - Hades</title>
     <meta name="description" content="Hades, master of Cerberus">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <link href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css" rel="stylesheet" type="text/css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script src="js/jquery.js"></script>
+    <link href="css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css">
     <link href="//fonts.googleapis.com/css?family=Calligraffitti|Ubuntu+Mono" rel="stylesheet" type="text/css">
     <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
