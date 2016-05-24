e907629b2dfb0fc82e56f5874d7fd73a7ec18373
Stefan Hüsges <tronsha@gmx.de>
2016-05-24 10:09:02 +0200

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm ./fonts/*
-cp ./vendor/fonts-master/apache/calligraffitti/*.ttf ./fonts/
-cp ./vendor/fonts-master/ufl/ubuntumono/*.ttf ./fonts/
+cp ./vendor/googlefonts/apache/calligraffitti/*.ttf ./fonts/
+cp ./vendor/googlefonts/ufl/ubuntumono/*.ttf ./fonts/
 cp ./vendor/font-awesome-4.6.*/fonts/* ./fonts/
 rm ./css/images/*
 cp ./vendor/jquery-ui-1.11.*/images/* ./css/images/
@@ -14,7 +14,7 @@ cat ./src/css/theme.css >> ./tmp.css
 java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./tmp.css -o ./css/style.min.css
 rm ./tmp.css
 echo "" > ./tmp.js
-cat ./vendor/jquery-dist-1.12.*/dist/jquery.js >> ./tmp.js
+cat ./vendor/jquery-1.12.*/dist/jquery.js >> ./tmp.js
 cat ./vendor/jquery-ui-1.11.*/jquery-ui.js >> ./tmp.js
 cat ./src/js/chat.js >> ./tmp.js
 java -jar ./vendor/yuicompressor-2.4.8.jar --type js ./tmp.js -o ./js/script.min.js
