6bcc47d93f5d338142ef156384ece17d8760a6cb
Stefan Hüsges <tronsha@gmx.de>
2016-09-12 08:41:01 +0200

    jQuery UI - v1.12.0

--- build.sh
+++ build.sh
@@ -4,18 +4,18 @@ cp ./vendor/googlefonts/apache/calligraffitti/*.ttf ./fonts/
 cp ./vendor/googlefonts/ufl/ubuntumono/*.ttf ./fonts/
 cp ./vendor/font-awesome-4.6.*/fonts/* ./fonts/
 rm ./css/images/*
-cp ./vendor/jquery-ui-1.11.*/images/* ./css/images/
+cp ./vendor/jquery-ui-1.12.*/images/* ./css/images/
 echo "" > ./tmp.css
 cat ./vendor/normalize.css-4.1.*/normalize.css >> ./tmp.css
 cat ./vendor/font-awesome-4.6.*/css/font-awesome.css >> ./tmp.css
-cat ./vendor/jquery-ui-1.11.*/jquery-ui.css >> ./tmp.css
+cat ./vendor/jquery-ui-1.12.*/jquery-ui.css >> ./tmp.css
 cat ./src/css/style.css >> ./tmp.css
 cat ./src/css/theme.css >> ./tmp.css
 java -jar ./vendor/yuicompressor-2.4.8.jar --type css ./tmp.css -o ./css/style.min.css
 rm ./tmp.css
 echo "" > ./tmp.js
 cat ./vendor/jquery-1.12.*/dist/jquery.js >> ./tmp.js
-cat ./vendor/jquery-ui-1.11.*/jquery-ui.js >> ./tmp.js
+cat ./vendor/jquery-ui-1.12.*/jquery-ui.js >> ./tmp.js
 cat ./src/js/chat.js >> ./tmp.js
 java -jar ./vendor/yuicompressor-2.4.8.jar --type js ./tmp.js -o ./js/script.min.js
 rm ./tmp.js
