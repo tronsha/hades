070ef53e38eb1550d1087bd032b951d614756693
Stefan Hüsges <tronsha@gmx.de>
2016-10-05 10:49:33 +0200

    added data:image

--- library/Hades.php
+++ library/Hades.php
@@ -239,6 +239,7 @@ class Hades
                 }
                 $value['text'] = $formatter->irc2html($value['text']);
                 $value['text'] = preg_replace('/https?:\/\/\S+\.(?:png|jpg|jpeg|gif)(\?\S+)?/i', '<img src="$0" alt="$0">', $value['text']);
+                $value['text'] = preg_replace('/data:image\/[a-z]+\;base64\,[a-zA-Z0-9\+\/]+\=*/i', '<img src="$0">', $value['text']);
                 $value['text'] = preg_replace('/https?:\/\/\S+\.(mp4|ogg)(\?\S+)?/i', '<video controls><source src="$0" type="video/$1"></video>', $value['text']);
                 $value['text'] = preg_replace('/(?:https:\/\/youtu.be\/)(.+)/i', '<iframe class="youtube" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
                 $value['text'] = preg_replace('/https:\/\/www.youtube.com\/watch\?(?:\S*?)?v\=([^\?&=]+)(?:\S*)/i', '<iframe class="youtube" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
