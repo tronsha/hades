bd0c7ab58bf96920dcab5ff9d604ec712730ee7b
Stefan Hüsges <tronsha@gmx.de>
2017-05-16 15:10:56 +0200

    update regex

--- library/Hades.php
+++ library/Hades.php
@@ -260,8 +260,8 @@ class Hades
                 $value['text'] = preg_replace('/data:(audio\/[a-z]+)\;base64\,[a-zA-Z0-9\+\/]+\=*/i', '<audio controls><source src="$0" type="$1"></audio>', $value['text']);
                 $value['text'] = preg_replace('/https?:\/\/\S+\.(mp4|ogg)(\?\S+)?/i', '<video controls><source src="$0" type="video/$1"></video>', $value['text']);
                 $value['text'] = preg_replace('/data:(video\/[a-z]+)\;base64\,[a-zA-Z0-9\+\/]+\=*/i', '<video controls><source src="$0" type="$1"></video>', $value['text']);
-                $value['text'] = preg_replace('/(?:https:\/\/youtu.be\/)(.+)/i', '<iframe class="youtube" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
-                $value['text'] = preg_replace('/https:\/\/www.youtube.com\/watch\?(?:\S*?)?v\=([^\?&=]+)(?:\S*)/i', '<iframe class="youtube" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
+                $value['text'] = preg_replace('/(?:https?:\/\/youtu.be\/)(\S+)/i', '<iframe class="youtube" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
+                $value['text'] = preg_replace('/https?:\/\/www.youtube.com\/watch\?(?:\S*?)?v\=([^\?&=]+)(?:\S*)/i', '<iframe class="youtube" src="https://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
                 $value['text'] = preg_replace('/https?:\/\/vimeo.com\/([0-9]+)(?:\S*)/i', '<iframe src="https://player.vimeo.com/video/$1?byline=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>', $value['text']);
                 $value['text'] = preg_replace('/https?:\/\/jsfiddle.net\/([^\.\s]+)/i', '<iframe class="jsfiddle" src="//jsfiddle.net/$1embedded/" frameborder="0" allowfullscreen></iframe>', $value['text']);
                 $value['text'] = preg_replace('/https?:\/\/pastebin.com\/(?:raw.php\?i\=)?([^\.\s\=]+)/i', '<iframe class="pastebin" src="http://pastebin.com/embed_iframe.php?i=$1" frameborder="0" allowfullscreen></iframe>', $value['text']);
