93eba8ade5f61bc409e0116e38415292cddd5dd7
Stefan Hüsges <tronsha@gmx.de>
2017-08-31 13:52:01 +0200

    text2bin

---
+++ src/js/functions.js
@@ -0,0 +1,12 @@
+function text2bin(text) {
+    var bin = '';
+    jQuery.each(text.split(""), function (i, x) {
+        var c = x.charCodeAt(0).toString(2);
+        var c0 = '';
+        for (var j = c.length; j < 8; j++) {
+            c0 += '0';
+        }
+        bin += c0 + c + ' ';
+    });
+    return bin;
+}
